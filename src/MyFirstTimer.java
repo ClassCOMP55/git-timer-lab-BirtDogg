d0c05c90ca86d61addd61176b9724cf112c51cb6
Roberto Garza-Castro <r_garzacastro@u.pacific.edu>
2026-02-20 11:52:52 -0800

    A Timer needs (1) a delay setting (between-event delay, and optionally an initial delay), (2) a listener to handle ticks (an ActionListener, e.g., implements ActionListener), and (3) start() to begin firing actionPerformed.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,23 +1,55 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener
+{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
-
-	public void init() {
+	private int numTimes;
+	Timer someTimerVar;
+	
+	public void init() 
+	{
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
 	
-	public void run() {
+	public void run() 
+	{
+		numTimes = 0;
+		someTimerVar = new Timer(1000, this);
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+//		Timer t = new Timer(1000, this);
+		someTimerVar.setInitialDelay(3000); // → delay before starting.
+		someTimerVar.start();
+		
 	}
 	
-	public static void main(String[] args) {
+	public void actionPerformed(ActionEvent e) 
+	{
+		//TODO add what we want to do every second
+		myLabel.move(5, 0);
+		numTimes += 1; 
+		myLabel.setLabel("times called? "+ numTimes);
+		if (numTimes == 10) 
+		{
+			someTimerVar.stop(); // → stops the timer.
+		}
+	}
+
+	
+	public static void main(String[] args) 
+	{
 		new MyFirstTimer().start();
 	}
 }
+
+//	class Name extends GraphicsProgram implements ActionListener {}
+
+//	public void actionPerformed(ActionEvent e) {}
+
