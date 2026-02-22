34d65a898799ce5d06cf1d2bd1e64390182bb9bf
Roberto Garza-Castro <r_garzacastro@u.pacific.edu>
2026-02-22 09:55:50 -0800

    enemies populated on screen regularly

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -13,84 +13,106 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class DodgeBall extends GraphicsProgram implements ActionListener {
+public class DodgeBall extends GraphicsProgram implements ActionListener 
+{
+	//variables
 	private ArrayList<GOval> balls;
 	private ArrayList<GRect> enemies;
 	private GLabel text;
-	private Timer movement;
+	private Timer moveTimer;
 	private RandomGenerator rgen;
+	private int numTimes;
+	//end variables
 	
+	//constants
 	public static final int SIZE = 25;
 	public static final int SPEED = 2;
 	public static final int MS = 50;
 	public static final int MAX_ENEMIES = 10;
 	public static final int WINDOW_HEIGHT = 600;
 	public static final int WINDOW_WIDTH = 300;
+	//end constants
 	
-	public void run() {
+	public void run() 
+	{
 		rgen = RandomGenerator.getInstance();
 		balls = new ArrayList<GOval>();
 		enemies = new ArrayList<GRect>();
+		numTimes = 0;
 		
 		text = new GLabel(""+enemies.size(), 0, WINDOW_HEIGHT);
 		add(text);
 		
-		movement = new Timer(MS, this);
-		movement.start();
+		moveTimer = new Timer(MS, this);
+		moveTimer.start();
 		addMouseListeners();
 	}
 	
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) 
+	{
+		numTimes ++;
+		if (numTimes % 40 == 0) addAnEnemy();
 		moveAllBallsOnce();
+		
 	}
 	
-	public void mousePressed(MouseEvent e) {
-		for(GOval b:balls) {
-			if(b.getX() < SIZE * 2.5) {
+	public void mousePressed(MouseEvent e) 
+	{
+		for(GOval b:balls) 
+		{
+			if(b.getX() < SIZE * 2.5) 
+			{
 				return;
 			}
 		}
 		addABall(e.getY());     
 	}
 	
-	private void addABall(double y) {
+	private void addABall(double y) 
+	{
 		GOval ball = makeBall(SIZE/2, y);
 		add(ball);
 		balls.add(ball);
 	}
 	
-	public GOval makeBall(double x, double y) {
+	public GOval makeBall(double x, double y) 
+	{
 		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
 		return temp;
 	}
 	
-	private void addAnEnemy() {
+	private void addAnEnemy() 
+	{
 		GRect e = makeEnemy(rgen.nextInt(0, WINDOW_HEIGHT-SIZE/2));
 		enemies.add(e);
 		text.setLabel("" + enemies.size());
 		add(e);
 	}
 	
-	public GRect makeEnemy(double y) {
+	public GRect makeEnemy(double y) 
+	{
 		GRect temp = new GRect(WINDOW_WIDTH-SIZE, y-SIZE/2, SIZE, SIZE);
 		temp.setColor(Color.GREEN);
 		temp.setFilled(true);
 		return temp;
 	}
 
-	private void moveAllBallsOnce() {
+	private void moveAllBallsOnce() 
+	{
 		for(GOval ball:balls) {
 			ball.move(SPEED, 0);
 		}
 	}
 	
-	public void init() {
+	public void init() 
+	{
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		new DodgeBall().start();
 	}
 }
